d61c260d47131872b8aa6ebad0222e69bf586381
Claas Augner <caugner@mozilla.com>
2025-03-24 21:48:20 +0100

    chore(deps-dev): add eslint-plugin-n

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import * as lit from "eslint-plugin-lit";
+import n from "eslint-plugin-n";
 import * as wc from "eslint-plugin-wc";
 
 export default defineConfig([
+  n.configs["flat/recommended"],
   wc.configs["flat/best-practice"],
   lit.configs["flat/all"],
   { files: ["**/*.{js,mjs,cjs}"] },
