6c7bc7b1deedd6914e8199b50902bf1338540f6d
Claas Augner <caugner@mozilla.com>
2025-03-26 14:32:37 +0100

    chore(deps-dev): add eslint-plugin-import

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import prettierConfig from "eslint-config-prettier/flat";
+// @ts-ignore
+import importPlugin from "eslint-plugin-import";
 import * as lit from "eslint-plugin-lit";
 import n from "eslint-plugin-n";
-import prettierConfig from "eslint-config-prettier/flat";
-import * as wc from "eslint-plugin-wc";
 import unicorn from "eslint-plugin-unicorn";
+import * as wc from "eslint-plugin-wc";
+import globals from "globals";
 
 export default defineConfig([
   n.configs["flat/recommended"],
@@ -34,6 +36,23 @@ export default defineConfig([
       "n/no-unsupported-features/node-builtins": ["off"],
       "unicorn/prevent-abbreviations": ["off"],
       "unicorn/template-indent": ["off"],
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { import: importPlugin },
+    rules: {
+      "sort-imports": "off",
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+          },
+          named: true,
+          "newlines-between": "always-and-inside-groups",
+        },
+      ],
     },
   },
   prettierConfig,
