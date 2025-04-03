f1fff0234eb685ee161ac7fd557bc55d7fd73614
Vadim Makeev <vmakeev@mozilla.com>
2025-04-03 16:29:15 +0200

    Fix layout: merge styles, put banner back (#35)

---
+++ components/banner/index.js
@@ -0,0 +1,7 @@
+import { html } from "lit";
+
+import "./index.css";
+
+export function Banner() {
+  return html`<div class="banner">Banner</div>`;
+}
--- components/page-layout/index.js
+++ components/page-layout/index.js
@@ -1,5 +1,8 @@
 import { html } from "lit";
 
+import "./index.css";
+
+import { Banner } from "../banner/index.js";
 import { BreadCrumbs } from "../breadcrumbs/index.js";
 import { Footer } from "../footer/index.js";
 import { Navigation } from "../navigation/index.js";
@@ -11,6 +14,7 @@ import { Navigation } from "../navigation/index.js";
 export function PageLayout(context, child) {
   return html`
     <body class="page-layout">
+      <div class="page-layout__banner">${Banner()}</div>
       <header class="page-layout__header">
         ${Navigation(context)} ${BreadCrumbs(context)}
       </header>
