f6cde2bb6411ac5e16614704c1620191b8266865
Vadim Makeev <vmakeev@mozilla.com>
2025-04-15 14:58:29 +0200

    Fix header’s dark scheme colors (#81)

--- components/quick-search/index.js
+++ components/quick-search/index.js
@@ -3,6 +3,11 @@ import { LitElement, css, html } from "lit";
 export class QuickSearch extends LitElement {
   static styles = css`
     .quick-search {
+      --quick-search-blue: light-dark(
+        var(--color-blue),
+        var(--color-blue-light)
+      );
+
       display: grid;
       grid-template-columns: 1fr min-content min-content;
       padding-inline: 0.75rem 3px;
@@ -14,7 +19,7 @@ export class QuickSearch extends LitElement {
 
       &:focus-within {
         width: 192px;
-        outline: 2px solid var(--color-blue);
+        outline: 2px solid var(--quick-search-blue);
         outline-offset: 2px;
       }
     }
@@ -25,6 +30,7 @@ export class QuickSearch extends LitElement {
       width: 100%;
       border: none;
       background-color: transparent;
+      font: inherit;
 
       &:focus {
         outline: none;
@@ -32,7 +38,7 @@ export class QuickSearch extends LitElement {
 
       &::placeholder {
         font-weight: bold;
-        color: var(--color-blue);
+        color: var(--quick-search-blue);
       }
     }
 
@@ -55,7 +61,11 @@ export class QuickSearch extends LitElement {
       }
 
       &:focus-visible {
-        outline: 2px solid var(--color-blue);
+        outline: 2px solid var(--quick-search-blue);
+      }
+
+      svg {
+        fill: currentcolor;
       }
     }
 
@@ -79,13 +89,7 @@ export class QuickSearch extends LitElement {
         class="quick-search__button quick-search__button--reset"
         type="reset"
       >
-        <svg
-          class="icon"
-          width="16"
-          height="16"
-          viewBox="-64 0 512 512"
-          aria-label="Clear"
-        >
+        <svg width="16" height="16" viewBox="-64 0 512 512" aria-label="Clear">
           <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
           <path
             d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
@@ -93,13 +97,7 @@ export class QuickSearch extends LitElement {
         </svg>
       </button>
       <button class="quick-search__button" type="submit">
-        <svg
-          class="icon"
-          width="16"
-          height="16"
-          viewBox="0 0 512 512"
-          aria-label="Send"
-        >
+        <svg width="16" height="16" viewBox="0 0 512 512" aria-label="Send">
           <!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
           <path
             d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
