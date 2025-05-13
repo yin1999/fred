c45548f2dee484f1f2b7ec139c724dd6d1b11644
Claas Augner <caugner@mozilla.com>
2025-05-13 13:43:21 +0200

    refactor(breadcrumbs): add divider via CSS

--- components/breadcrumbs/server.js
+++ components/breadcrumbs/server.js
@@ -2,8 +2,6 @@ import { html, nothing } from "lit";
 
 import { ServerComponent } from "../server/index.js";
 
-import dividerSvg from "./divider.svg?lit";
-
 export class Breadcrumbs extends ServerComponent {
   /**
    * @param {Fred.Context} context
@@ -16,9 +14,8 @@ export class Breadcrumbs extends ServerComponent {
     return html`
       <ul class="breadcrumbs">
         ${context.doc.parents.map(
-          ({ uri, title }, index) => html`
+          ({ uri, title }) => html`
             <li>
-              ${index > 0 ? dividerSvg : nothing}
               <a href=${uri}>${title}</a>
             </li>
           `,
