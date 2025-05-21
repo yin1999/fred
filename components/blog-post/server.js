228da9037488a02c4e19369e4410a6ef4c180431
Leo McArdle <leo@mca.is>
2025-05-21 15:38:51 +0000

    fix(blog-post): add sidebar placement

--- components/blog-post/server.js
+++ components/blog-post/server.js
@@ -24,31 +24,6 @@ function BlogTitleImageFigure(_context, { image, width, height }) {
 }
 
 /**
- * @param {Fred.Context<Rari.BlogPage>} context
- * @param {object} params
- * @param {Rari.TocEntry[] | undefined} params.toc
- * @returns {Lit.TemplateResult | nothing}
- */
-function BlogPostToc(context, { toc }) {
-  if (!toc || toc.length === 0) {
-    return nothing;
-  }
-
-  return html`
-    <div class="blog-post__toc">${ReferenceToc.render(context)}</div>
-  `;
-}
-
-/**
- * @param {Fred.Context} _context
- * @returns {Lit.TemplateResult | nothing}
- */
-function SidePlacement(_context) {
-  // TODO: implement somewhere central
-  return nothing;
-}
-
-/**
  * @param {Fred.Context} context
  * @param {object} params
  * @param {Rari.BlogPostDoc} params.doc
@@ -111,12 +86,12 @@ export class BlogPost extends ServerComponent {
         </p>`,
       );
     }
-    const { toc } = doc;
 
     const postContent = html`
       <article class="blog-post__main">
         <aside class="blog-post__sidebar">
-          ${BlogPostToc(context, { toc })} ${SidePlacement(context)}
+          ${ReferenceToc.render(context)}
+          <mdn-placement-sidebar></mdn-placement-sidebar>
         </aside>
         <div class="blog-post__content">
           <header class="blog-post__header">
