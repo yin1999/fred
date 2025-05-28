5927944cf67af1cf0b547d47239d425d914b6ae0
Claas Augner <caugner@mozilla.com>
2025-05-28 17:54:39 +0200

    feat(home-page): add latest-news

--- components/home-page/server.js
+++ components/home-page/server.js
@@ -2,6 +2,7 @@ import { html } from "lit";
 
 import { FeaturedArticles } from "../featured-articles/server.js";
 import { HomepageHero } from "../homepage-hero/server.js";
+import { LatestNews } from "../latest-news/server.js";
 import { PageLayout } from "../page-layout/server.js";
 import { RecentContributions } from "../recent-contributions/server.js";
 import { ServerComponent } from "../server/index.js";
@@ -22,6 +23,13 @@ export class HomePage extends ServerComponent {
           <section class="homepage-section">
             <h2>${context.l10n`Featured articles`}</h2>
             ${FeaturedArticles.render(context.hyData.featuredArticles)}
+          </section>
+          <section class="homepage-section">
+            <h2>${context.l10n`Latest news`}</h2>
+            ${LatestNews.render(
+              context.hyData.latestNews.items,
+              context.locale,
+            )}
           </section>
           <section class="homepage-section">
             <h2>${context.l10n`Recent contributions`}</h2>
---
+++ components/latest-news/server.js
@@ -0,0 +1,69 @@
+import { html, nothing } from "lit";
+
+import { LoremIpsum } from "../lorem-ipsum/pure.js";
+import { ServerComponent } from "../server/index.js";
+
+export class LatestNews extends ServerComponent {
+  /**
+   * @param {import("@rari").HomePageLatestNewsItem[]} items
+   * @param {string} locale
+   */
+  render(items, locale) {
+    const rtf = new Intl.RelativeTimeFormat(locale);
+    const now = Date.now();
+
+    return html`<ul class="latest-news">
+      ${items.map(
+        (item) =>
+          html`<li>
+            <article class="latest-news__item">
+              ${item.source
+                ? html`<a class="latest-news__source" href=${item.source.url}
+                    >${item.source.name}</a
+                  >`
+                : nothing}
+              <h3 class="latest-news__title">
+                <a href=${item.url}>${item.title}</a>
+              </h3>
+              <time class="latest-news__date" datetime=${item.published_at}>
+                ${rtf.format(
+                  ...getRelativeTime(
+                    (Date.parse(item.published_at) - now) / 1000,
+                  ),
+                )}
+              </time>
+              <p class="latest-news__summary">${LoremIpsum()}</p>
+            </article>
+          </li>`,
+      )}
+    </ul>`;
+  }
+}
+
+/**
+ *
+ * @param {number} seconds
+ * @returns {[number, unit: Intl.RelativeTimeFormatUnit]}
+ */
+function getRelativeTime(seconds) {
+  /** @type {Array<{amount: number, unit: Intl.RelativeTimeFormatUnit}>} */
+  const divisions = [
+    { amount: 60, unit: "second" },
+    { amount: 60, unit: "minute" },
+    { amount: 24, unit: "hour" },
+    { amount: 7, unit: "day" },
+    { amount: 4.345_24, unit: "week" }, // 365/12/7
+    { amount: 12, unit: "month" },
+    { amount: Number.POSITIVE_INFINITY, unit: "year" },
+  ];
+
+  let duration = seconds;
+  for (const division of divisions) {
+    if (Math.abs(duration) < division.amount) {
+      return [Math.round(duration), division.unit];
+    }
+    duration /= division.amount;
+  }
+
+  return [0, "year"];
+}
