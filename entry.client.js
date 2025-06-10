28bfff5fe24a21711c20e439cbd27d15a99bb49e
Claas Augner <caugner@mozilla.com>
2025-06-10 19:07:43 +0200

    feat(toc): highlight current sections

--- entry.client.js
+++ entry.client.js
@@ -5,6 +5,7 @@ import "@lit-labs/ssr-client/lit-element-hydrate-support.js";
 import "./hooks/glean-init.js";
 import "./hooks/load-elements.js";
 import "./components/navigation/hook.js";
+import "./hooks/toc-highlight.js";
 import "./hooks/code-examples.js";
 import "./hooks/live-samples.js";
 import "./hooks/css-formal-syntax.js";
---
+++ hooks/toc-highlight.js
@@ -0,0 +1,74 @@
+/**
+ * @param {HTMLElement} toc
+ * @returns {void}
+ */
+function highlightTOC(toc) {
+  // 1. Collect TOC items
+  const tocItems = [...toc.querySelectorAll("a")];
+
+  // 2. Map sections to TOC item
+  /** @type {Map<HTMLElement, HTMLAnchorElement>} */
+  const tocItemBySection = new Map();
+  for (const item of tocItems.reverse()) {
+    const target = document.querySelector(item.hash);
+
+    if (!target) {
+      continue;
+    }
+
+    /** @type {Element|null} */
+    let section = target.closest("section");
+
+    while (
+      section &&
+      section instanceof HTMLElement &&
+      !tocItemBySection.has(section)
+    ) {
+      tocItemBySection.set(section, item);
+      const next = section.nextElementSibling;
+      section = next;
+    }
+  }
+
+  /** @type {WeakMap<HTMLElement,number>} */
+  const visibleCounts = new WeakMap();
+
+  // 3. Handle visibility transitions
+  const onIntersect = (/** @type {IntersectionObserverEntry[]} */ entries) => {
+    for (const entry of entries) {
+      if (!(entry.target instanceof HTMLElement)) {
+        continue;
+      }
+      const item = tocItemBySection.get(entry.target);
+      if (!item) {
+        continue;
+      }
+
+      let visibleCount = visibleCounts.get(item);
+      if (typeof visibleCount === "number") {
+        visibleCount += entry.isIntersecting ? 1 : -1;
+        item.ariaCurrent = visibleCount > 0 ? "true" : null;
+        visibleCounts.set(item, visibleCount);
+      } else if (entry.isIntersecting) {
+        item.ariaCurrent = "true";
+        visibleCounts.set(item, 1);
+      }
+    }
+  };
+
+  // 4. Init observer
+  const observer = new IntersectionObserver(onIntersect, {
+    threshold: 0,
+  });
+
+  // 5. Observe sections
+  for (const section of tocItemBySection.keys()) {
+    observer.observe(section);
+  }
+}
+
+const toc = document.querySelector(".reference-toc");
+
+if (toc instanceof HTMLElement) {
+  highlightTOC(toc);
+}
