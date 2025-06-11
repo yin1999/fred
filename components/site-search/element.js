6b9cd66521d84b305e9f3796bb2b7861b02b3d6e
Claas Augner <caugner@mozilla.com>
2025-06-11 13:15:12 +0200

    feat(site-search): add "Language" options

--- components/site-search/element.js
+++ components/site-search/element.js
@@ -1,5 +1,5 @@
 import { Task } from "@lit/task";
-import { LitElement, html } from "lit";
+import { LitElement, html, nothing } from "lit";
 
 import { L10nMixin } from "../../l10n/mixin.js";
 
@@ -16,22 +16,30 @@ export class MDNSiteSearch extends L10nMixin(LitElement) {
     super();
     /** @type {string | undefined} */
     this._query = undefined;
+    /** @type {string[]} */
+    this._locales = [this.locale];
     /** @type {string} */
     this._sort = "";
     this._page = 1;
   }
 
   _searchTask = new Task(this, {
-    args: () => [this._query, this._sort, this._page],
-    task: async ([query, sort, page], { signal }) => {
+    args: () => [this._query, this._locales, this._sort, this._page],
+    task: async ([query, locales, sort, page], { signal }) => {
       if (!query) {
         return;
       }
+
       const params = new URLSearchParams({
         q: query,
         sort,
         page: page.toString(),
       });
+
+      for (const locale of locales) {
+        params.append("locale", locale);
+      }
+
       const res = await fetch(`/api/v1/search?${params}`, { signal });
       if (!res.ok) {
         throw new Error(`${res.status}: ${res.statusText}`);
@@ -46,14 +54,25 @@ export class MDNSiteSearch extends L10nMixin(LitElement) {
     super.connectedCallback();
     const params = new URLSearchParams(location.search);
     this._query = params.get("q") || undefined;
+    this._locales = params.has("locale")
+      ? params.getAll("locale").sort()
+      : [this.locale];
     this._sort = params.get("sort") || "best";
+
     const page = params.get("page");
+
     if (page) {
       this._page = Number.parseInt(page, 10);
     }
   }
 
   render() {
+    /** @type {Array<string[]>} */
+    const LOCALE_OPTIONS =
+      this.locale === "en-US"
+        ? []
+        : [[this.locale], ["en-US"], [this.locale, "en-US"].sort()];
+
     /** @type {[string, string][]} */
     const SORT_OPTIONS = [
       ["best", this.l10n`Best`],
@@ -73,6 +92,29 @@ export class MDNSiteSearch extends L10nMixin(LitElement) {
                 })}
               </h1>
               <section class="site-search__options">
+                ${
+                  LOCALE_OPTIONS.length > 0
+                    ? html` <h2>${this.l10n`Language`}</h2>
+                        <ul>
+                          ${LOCALE_OPTIONS.map((locales) => {
+                            const label =
+                              locales.length == 1
+                                ? locales.at(0)
+                                : this.l10n`Both`;
+                            if (this._locales.join(",") === locales.join(",")) {
+                              return html`<li><em>${label}</em></li>`;
+                            } else {
+                              const url = new URL(location.href);
+                              url.searchParams.delete("locale");
+                              for (const locale of locales) {
+                                url.searchParams.append("locale", locale);
+                              }
+                              return html`<li><a href=${url}>${label}</a></li>`;
+                            }
+                          })}
+                        </ul>`
+                    : nothing
+                }
                 <h2>${this.l10n`Sort by`}</h2>
                 <ul>
                 ${SORT_OPTIONS.map(([sort, label]) => {
