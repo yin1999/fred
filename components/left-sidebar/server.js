4f29a228710d6a95ca3437bfd6b699213030f36e
Claas Augner <caugner@mozilla.com>
2025-06-18 17:19:40 +0200

    fix(left-sidebar): hide filter if empty

--- components/left-sidebar/server.js
+++ components/left-sidebar/server.js
@@ -1,4 +1,4 @@
-import { html } from "lit";
+import { html, nothing } from "lit";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
 
 import { ServerComponent } from "../server/index.js";
@@ -8,11 +8,15 @@ export class LeftSidebar extends ServerComponent {
    * @param {import("@fred").Context<import("@rari").DocPage>} context
    */
   render(context) {
+    const content = context?.doc?.sidebarHTML;
+
+    if (!content) {
+      return nothing;
+    }
+
     return html`<nav class="left-sidebar">
       <mdn-sidebar-filter></mdn-sidebar-filter>
-      <section class="left-sidebar--content">
-        ${unsafeHTML(context?.doc?.sidebarHTML)}
-      </section>
+      <section class="left-sidebar--content">${unsafeHTML(content)}</section>
     </nav>`;
   }
 }
