53f04648b6b70bfa55134c722b9cae7a6b03b561
Claas Augner <495429+caugner@users.noreply.github.com>
2025-07-09 20:32:41 +0200

    enhance(dropdown): close on escape (#349)
    * enhance(dropdown): close on escape
    
    * chore(dropdown): close on global escape

--- components/dropdown/element.js
+++ components/dropdown/element.js
@@ -54,6 +54,13 @@ export class MDNDropdown extends LitElement {
     }
   }
 
+  /** @param {KeyboardEvent} event */
+  _globalKeyDown(event) {
+    if (this.open && event.key === "Escape") {
+      this._toggleDropDown();
+    }
+  }
+
   _toggleDropDown() {
     this.open = !this.open;
   }
@@ -76,6 +83,8 @@ export class MDNDropdown extends LitElement {
     super.connectedCallback();
     this._globalClick = this._globalClick.bind(this);
     document.addEventListener("click", this._globalClick);
+    this._globalKeyDown = this._globalKeyDown.bind(this);
+    document.addEventListener("keydown", this._globalKeyDown);
   }
 
   render() {
@@ -96,6 +105,7 @@ export class MDNDropdown extends LitElement {
   disconnectedCallback() {
     super.disconnectedCallback();
     document.removeEventListener("click", this._globalClick);
+    document.removeEventListener("keydown", this._globalKeyDown);
   }
 }
 
