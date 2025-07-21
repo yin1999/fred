5972d79a53a03abb9b6f6aa6dc83ae91dc69cf2f
Leo McArdle <leo@mca.is>
2025-07-21 12:20:07 +0100

    fix(search): close when clicking backdrop cross-browser (#436)

--- entry.client.js
+++ entry.client.js
@@ -3,6 +3,7 @@ import "@lit-labs/ssr-client/lit-element-hydrate-support.js";
 
 // hooks:
 import "./hooks/glean-init.js";
+import "./hooks/dialog-closedby.js";
 import "./hooks/load-elements.js";
 import "./hooks/sidebar-scroll-to-current.js";
 import "./components/navigation/hook.js";
