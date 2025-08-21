dee9843ff7ee9b531807f6388585b89ebe80b0cc
Claas Augner <495429+caugner@users.noreply.github.com>
2025-08-21 10:06:30 +0200

    fix(baseline-indicator): remove asterisk artifact (#596)

--- components/baseline-indicator/server.js
+++ components/baseline-indicator/server.js
@@ -204,7 +204,7 @@ export class BaselineIndicator extends ServerComponent {
               </p>`
             : html`<p>${context.l10n("baseline-not-extra")}</p>`}
         ${status.asterisk
-          ? html`<p>{"* "}${context.l10n("baseline-asterisk")}</p>`
+          ? html`<p>* ${context.l10n("baseline-asterisk")}</p>`
           : nothing}
         <ul>
           <li>
