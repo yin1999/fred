e6df81e0e03c2a098dfbb509484f9ce85c95adfe
Leo McArdle <leo@mozilla.com>
2025-08-28 16:10:50 +0100

    feat(baseline): make open state sticky

---
+++ components/baseline-indicator/hook.js
@@ -0,0 +1,18 @@
+const indicator = document.querySelector(".baseline-indicator");
+if (indicator instanceof HTMLDetailsElement) {
+  indicator.addEventListener("toggle", () => {
+    saveState(indicator.open);
+  });
+  saveState(indicator.open);
+}
+
+/**
+ * @param {boolean} open
+ */
+function saveState(open) {
+  if (open) {
+    localStorage.setItem("baseline-indicator", "open");
+  } else {
+    localStorage.removeItem("baseline-indicator");
+  }
+}
--- entry.inline.js
+++ entry.inline.js
@@ -12,3 +12,35 @@ try {
 } catch (error) {
   console.warn("Unable to set nop", error);
 }
+
+try {
+  if (localStorage.getItem("baseline-indicator") === "open") {
+    /** @param {HTMLDetailsElement} indicator */
+    const open = (indicator) => {
+      observer.disconnect();
+      indicator.open = true;
+    };
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+          if (
+            node instanceof HTMLDetailsElement &&
+            node.classList.contains("baseline-indicator")
+          ) {
+            open(node);
+          }
+        }
+      }
+    });
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    });
+    const indicator = document.querySelector(".baseline-indicator");
+    if (indicator instanceof HTMLDetailsElement) {
+      open(indicator);
+    }
+  }
+} catch (error) {
+  console.warn("Unable to set baseline indicator state", error);
+}
