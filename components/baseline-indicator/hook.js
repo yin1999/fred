7d4edfb533cec2a5aaf94f93c2ea90f8225d5055
Leo McArdle <leo@mozilla.com>
2025-09-01 12:42:44 +0100

    review updates

--- components/baseline-indicator/hook.js
+++ components/baseline-indicator/hook.js
@@ -1,18 +1,22 @@
-const indicator = document.querySelector(".baseline-indicator");
-if (indicator instanceof HTMLDetailsElement) {
-  indicator.addEventListener("toggle", () => {
+try {
+  const indicator = document.querySelector(".baseline-indicator");
+  if (indicator instanceof HTMLDetailsElement) {
+    indicator.addEventListener("toggle", () => {
+      saveState(indicator.open);
+    });
     saveState(indicator.open);
-  });
-  saveState(indicator.open);
-}
-
-/**
- * @param {boolean} open
- */
-function saveState(open) {
-  if (open) {
-    localStorage.setItem("baseline-indicator", "open");
-  } else {
-    localStorage.removeItem("baseline-indicator");
   }
+
+  /**
+   * @param {boolean} open
+   */
+  function saveState(open) {
+    if (open) {
+      localStorage.setItem("baseline-indicator", "open");
+    } else {
+      localStorage.removeItem("baseline-indicator");
+    }
+  }
+} catch (error) {
+  console.warn("Unable to attach to baseline indicator", error);
 }
