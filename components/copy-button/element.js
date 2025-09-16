44442fec2d61ae3d68dffacbd6f0c82765d7fead
Onkar Khadangale <87750369+OnkarRuikar@users.noreply.github.com>
2025-09-16 14:50:48 +0530

    chore(copy-button): add checkmark to copied indicator (#673)

--- components/copy-button/element.js
+++ components/copy-button/element.js
@@ -2,16 +2,19 @@ import { LitElement, html } from "lit";
 
 import { L10nMixin } from "../../l10n/mixin.js";
 import { MDNButton } from "../button/element.js";
+import check from "../icon/check.svg?lit";
 
 export class MDNCopyButton extends L10nMixin(LitElement) {
   static properties = {
     variant: {},
     _message: { state: true },
+    _copiedSuccessfully: { state: true },
   };
 
   constructor() {
     super();
     this.variant = "primary";
+    this._copiedSuccessfully = false;
     /** @type {Element | undefined} */
     this.copiesFrom = undefined;
   }
@@ -21,35 +24,39 @@ export class MDNCopyButton extends L10nMixin(LitElement) {
    */
   async _copy({ target }) {
     if (target instanceof MDNButton) {
-      let copiedSuccessfully = true;
       try {
         const text = this.copiesFrom?.textContent?.trim();
         if (text) {
           await navigator.clipboard.writeText(text);
+          this._copiedSuccessfully = true;
         }
       } catch (error) {
         console.error(
           "Error when trying to use navigator.clipboard.writeText()",
           error,
         );
-        copiedSuccessfully = false;
+        this._copiedSuccessfully = false;
       }
 
-      this._message = copiedSuccessfully
+      this._message = this._copiedSuccessfully
         ? this.l10n`Copied`
         : this.l10n`Copy failed!`;
 
       setTimeout(
         () => {
           this._message = undefined;
+          this._copiedSuccessfully = false;
         },
-        copiedSuccessfully ? 1000 : 3000,
+        this._copiedSuccessfully ? 1000 : 3000,
       );
     }
   }
 
   render() {
-    return html`<mdn-button @click=${this._copy} variant=${this.variant}
+    return html`<mdn-button
+      @click=${this._copy}
+      .icon=${this._copiedSuccessfully ? check : undefined}
+      variant=${this.variant}
       >${this._message ?? this.l10n`Copy`}</mdn-button
     >`;
   }
