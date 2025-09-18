51a6f25adcd414da351296f3748b32db8cf48f3c
Claas Augner <495429+caugner@users.noreply.github.com>
2025-09-18 10:35:55 +0200

    feat(glean): measure external link clicks (#787)

--- hooks/glean-init.js
+++ hooks/glean-init.js
@@ -31,12 +31,28 @@ Glean.initialize(GLEAN_APP_ID, uploadEnabled, {
 document.addEventListener("click", (event) => {
   const composedTarget = event.composedPath()?.[0];
   if (composedTarget !== event.target && composedTarget instanceof Element) {
+    // Workaround for automatic click events in shadow DOM.
+    // See: https://bugzil.la/1988206
     const taggedElement = composedTarget.closest("[data-glean-id]");
     if (taggedElement instanceof HTMLElement) {
       const gleanId = taggedElement.dataset.gleanId;
       if (gleanId) {
         gleanClick(gleanId);
       }
+    }
+  }
+
+  const target = composedTarget ?? event.target;
+  if (target instanceof Element) {
+    // External link measurement.
+    const anchor = target.closest("a");
+    if (
+      anchor instanceof HTMLAnchorElement &&
+      anchor.href &&
+      anchor.origin &&
+      anchor.origin !== document.location.origin
+    ) {
+      gleanClick(`external-link: ${anchor.href}`);
     }
   }
 });
