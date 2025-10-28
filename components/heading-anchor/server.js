d4731566badf9ef2b215253bfe85d802f0b8873c
Andi Pieper <apieper@mozilla.com>
2025-10-28 10:47:53 +0100

    fix(content-section): loosen restrictions on heading ids (#1008)
    loosen restrictions on element ids

--- components/heading-anchor/server.js
+++ components/heading-anchor/server.js
@@ -11,8 +11,6 @@ export class HeadingAnchor extends ServerComponent {
    * @param {string} title
    */
   render(level, id, title) {
-    // remove leading numbers, dots, underscores, and commas to form a valid element id
-    id = id ? id.replace(/^[0-9._,]+/, "") : id;
     return id
       ? hh`<${unsafeStatic("h" + level)} id=${ifDefined(id)} class="heading"><a class="heading-anchor" href="#${id}">${unsafeStatic(title)}</a></${unsafeStatic("h" + level)}>`
       : nothing;
